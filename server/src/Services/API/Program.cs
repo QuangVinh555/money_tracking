c8d9cc5d0707506ddf4a8140d43981833209783d
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2025-07-03 22:24:48 +0700

    [Update] Update enviroment swagger

--- server/src/Services/API/Program.cs
+++ server/src/Services/API/Program.cs
@@ -110,11 +110,14 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Sử dụng CORS
 app.UseCors("AllowAll");
